3ab59c7459503bcfd50407221244405d34d597c6
M.Rieck <michael.rieck@neusoft.com>
2013-06-17 16:46:21 +0200

    Update due to changed localization

---
+++ LocalizationTest.cs
@@ -0,0 +1,42 @@
+using System;
+using Docking.Components;
+using System.Reflection;
+using System.Resources;
+using System.Collections;
+using System.ComponentModel.Design;
+using Docking;
+
+namespace Examples
+{
+
+   [System.ComponentModel.ToolboxItem(false)]
+   public partial class LocalizationTest : Gtk.Bin, ILocalizable
+   {
+      public LocalizationTest()
+      {
+         this.Build();
+         this.Name = "Localization";
+      }
+
+      void ILocalizable.LocalizationChanged(DockItem item)
+      {
+         // TODO: next line will be moved to a generic solution soon
+         this.Name = "Localization".Localized(GetType().Namespace);
+      }
+   }
+   
+   #region Starter / Entry Point
+
+   public class LocalizationTestFactory : ComponentFactory
+   {
+      public override Type TypeOfInstance { get { return typeof(LocalizationTest); } }
+      public override String MenuPath { get { return @"View\Examples\Localization Test"; } }
+      public override String Comment { get { return "Localization example"; } }
+      public override Mode Options { get { return Mode.CloseOnHide; } }
+      public override Gdk.Pixbuf Icon { get { return Gdk.Pixbuf.LoadFromResource ("Examples.HelloWorld-16.png"); } }
+   }
+
+   #endregion
+
+}
+
