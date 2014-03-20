c21f6c53ee14e17aad596722b60264970097fef3
S.Lohse <steffen.lohse@neusoft.com>
2014-03-20 21:47:10 +0100

    working on scripting

--- Python/test1.py
+++ Python/test1.py
@@ -1,4 +1,4 @@
-# simpliest python program, print a message on console
+# the most simple python program, print a message on console
 # therefore you can't see any effect when executing except in debugger 
 
 print 'Hello Python World'
